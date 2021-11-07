7794146bfedb8e70145f85c6d0707b8b9aa4a42d
Michelle <michellehnguyen03@gmail.com>
2021-11-07 13:39:04 -0600

    made routes related to our user

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 const express = require('express')
 const app = express() // creates an application which allows us to set up our entire server
 
+app.set('view engine', 'ejs')
 app.get('/', (req, res)=> {
     console.log('Here')
     // res.download('server.js') // downloads something to the client
     // res.json({message: "Error"}) // 500 sends error on server, you should send a message with it
     // res.send("Hi")  // send is used for testing purposes, but it's very general
-    res.render('index') // renders a file and passes it in the path in the file you want to render
+    res.render('index', { text: "World"}) // renders a file and passes it in the path in the file you want to render
 })
- 
+ // these are routes related to our user
+app.get('/users', (req, res) => {
+    res.send("User List")
+})
+
+app.get('/users/new', (req, res) => {
+    res.send("User New Form")
+})
 app.listen(3000)
